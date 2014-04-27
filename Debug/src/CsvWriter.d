1b3977f4791a85daf60edb6d0fca1f42f662bdfa
edgarriba <edgar.riba@gmail.com>
2014-04-27 18:14:23 +0200

    Remove Debug/ folder

--- Debug/src/CsvWriter.d
+++
@@ -1,3 +0,0 @@
-src/CsvWriter.d: ../src/CsvWriter.cpp ../src/CsvWriter.h
-
-../src/CsvWriter.h:
--- Debug/src/ObjectModel.d
+++
@@ -1,3 +0,0 @@
-src/ObjectModel.d: ../src/ObjectModel.cpp ../src/ObjectModel.h
-
-../src/ObjectModel.h:
